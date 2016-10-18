9c4e383c281badcd7671874e34a7e9379497548f
thomas <thomas.karl.richter@gmail.com>
2016-10-18 15:57:44 +0000

    date to eventList added

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-//var fetch = require("fetch");
-//var select = require('soupselect').select;
-//// dom provided by htmlparser...
-//// select(dom, "#main a.article").forEach(function(element) {//...});
-//var parse = require("query-dom").parse;
-//var dom;
-//fetch.fetchUrl("http://www.stadtrevue.de/tageskalender/tageskalender-results/", function(error, meta,body) {
-   //dom = parse(body.toString());
-   //console.log(dom);
-   //console.log(select(dom,"a"));//.forEach(function(element){ console.log(element.toString());});
-//});
-
 var jsdom = require("jsdom");
 var moment = require("moment");
 var myday = moment();
 var eventList = [];
 var object = {};
 var CAT = "konzerte";
-var dayOfMonth = 32;
-    //"http://www.stadtrevue.de/tageskalender/tageskalender-results/",
+var dayOfMonth = 3;
 var fetchPage = function(counter, url) {
   jsdom.env(
     url+CAT+myday.format("/YYYY/MM/DD/"),
@@ -26,18 +13,11 @@ var fetchPage = function(counter, url) {
       var root = window.$(".tx-srtk-pi1-listrow");
       root.children().map(function(number, item) {
         var cat = item.classList["0"];
-        /*              if(cat && (cat.endsWith("name")
-          || cat.endsWith("rubric")
-            || cat.endsWith("date")
-              || cat.endsWith("time")
-                || cat.endsWith("title")
-                  || cat.endsWith("text")) ){
-                  console.log(number, item.textContent);
-                  if(cat.endsWith("text")) console.log("\n");
-                  }
-                  */
         if(cat) {
-          if(cat.endsWith("date")) object.date=item.textContent;
+          if(cat.endsWith("date"))
+            object.date=item.textContent;
+          else
+            object.date=myday.format("DD.MM.YYYY");
           if(cat.endsWith("time")) object.time=item.textContent;
           if(cat.endsWith("rubric")) object.rubric=item.textContent;
           if(cat.endsWith("name")) object.name=item.textContent;
@@ -51,6 +31,15 @@ var fetchPage = function(counter, url) {
       });
         if(counter === 0){
           console.log("on "+myday.format("/YYYY/MM/DD/")+" "+eventList.length+" events recorded");
+          require('fs').writeFile(
+            './events.json',
+            JSON.stringify(eventList),
+            function (err) {
+              if (err) {
+                console.error('Crap happens');
+              }
+            }
+          );
         } else {
           console.log("on "+myday.format("/YYYY/MM/DD/")+" "+eventList.length+" events recorded");
           counter--;
@@ -61,4 +50,4 @@ var fetchPage = function(counter, url) {
 }
 //http://www.stadtrevue.de/tageskalender/tageskalender-results/konzerte/2016/10/5/
 fetchPage(dayOfMonth, "http://www.stadtrevue.de/tageskalender/tageskalender-results/");
-console.log("ready");
+console.log("begin fetching...");
