489baf01e6ba8a02c0554bb572de21fa4874d6de
Thomas Richter <thomas.karl.richter@gmail.com>
2016-11-09 14:04:22 +0000

    objects global

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
 var commandLineArgs = require("command-line-args");
 var options = commandLineArgs([
   {name: "tage", alias: "t", type: Number},
-  {name: "datum", alias: "d", type: String}
+  {name: "datum", alias: "d", type: String},
+  {name: "cats", alias: "c", type: String},
+  {name: "url", alias: "u", type: String}
 ]);
 console.log(JSON.stringify(options));
 var jsdom = require("jsdom");
 var moment = require("moment");
 var myday = moment(options.datum);
 var eventsList = [];
-var startCAT = "konzerte";
+var startCAT = "kinder";
 var l = [];
-var includedCat = /(kinder|konzerte)/;
+var object = {};
+var includedCat = options.cats ? new RegExp(options.cats) : new RegExp("(kinder|konzerte)");
 
 var fetchDatePage = function(counter, url, CAT, date) {
+	if(counter === 0){
+		console.log("on "+date.format("/YYYY/MM/DD/")+" "+eventsList.length+" events recorded");
+		console.log(JSON.stringify(eventsList, null, 1));
+		require('fs').writeFile(
+			'./events.json',
+			JSON.stringify(eventsList, null, 2),
+			function (err) {
+				if (err) {
+					console.error('Fehler beim Schreiben der events-JSON-Datei');
+				}
+			}
+		);
+		return;
+	} else {
   jsdom.env(
-    url+CAT+date.format("/YYYY/MM/DD/"),
+    url + CAT + date.format("/YYYY/MM/DD/"),
     ["http://code.jquery.com/jquery.js"],
     function (err, window) {
       var root = window.$(".tx-srtk-pi1-listrow");
       root.children().map(function(number, item) {
-        var object = {};
         var cat = item.classList["0"];
+        console.log("--->",cat);
         if(cat) {
           if(cat.endsWith("date"))
             object.date=item.textContent;
           else
             object.date=myday.format("DD.MM.YYYY");
-          if(cat.endsWith("time")) object.time=item.textContent;
-          if(cat.endsWith("rubric")) object.rubric=item.textContent;
-          if(cat.endsWith("name")) object.name=item.textContent;
-          if(cat.endsWith("title")) object.title=item.textContent;
-          if(cat.endsWith("text")) {
+          if(cat.endsWith("time")) 
+            object.time=item.textContent;
+          else if(cat.endsWith("rubric")) 
+            object.rubric=item.textContent;
+          else if(cat.endsWith("name")) 
+            object.name=item.textContent;
+          else if(cat.endsWith("title")) 
+            object.title=item.textContent;
+          else if(cat.endsWith("text")) {
             object.text=item.textContent;
             eventsList.push(object);
+            object = {};
           }
         }
       });
@@ -46,30 +68,20 @@ var fetchDatePage = function(counter, url, CAT, date) {
         console.log(l);
       }
       if(l.length > 0){
-        //console.log("on "+CAT.split("/")[5]+ " "+myday.format("/YYYY/MM/DD/")+" "+eventList.length+" events recorded");
         fetchDatePage(counter, url, l.shift(), date);
       } else {
-        if(counter === 0){
-          console.log("on "+date.format("/YYYY/MM/DD/")+" "+eventsList.length+" events recorded");
-          console.log(JSON.stringify(eventsList));
-          require('fs').writeFile(
-            './events.json',
-            JSON.stringify(eventsList, null, 2),
-            function (err) {
-              if (err) {
-                console.error('Fehler beim Schreiben der events-JSON-Datei');
-              }
-            }
-          );
-        } else {
           console.log("on "+date.format("/YYYY/MM/DD/")+" "+eventsList.length+" events recorded");
           counter--;
           date.add(1,"day");
           fetchDatePage(counter, url, CAT, date);
-          }
       }
     });
+  }
 };
-//http://www.stadtrevue.de/tageskalender/tageskalender-results/konzerte/2016/10/5/
-fetchDatePage(options.tage || 1, "http://www.stadtrevue.de/tageskalender/tageskalender-results/", startCAT, myday);
+fetchDatePage(
+	options.tage || 1, 
+	options.url || "http://www.stadtrevue.de/tageskalender/tageskalender-results/", 
+	startCAT, 
+	myday);
+
 console.log("begin fetching...");
