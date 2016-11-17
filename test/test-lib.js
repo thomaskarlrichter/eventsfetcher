36fcf4b090f939438d4406777d323abb4c7f222c
Thomas Richter <thomas.karl.richter@gmail.com>
2016-11-17 15:29:59 +0000

    tests with Erw.: added

--- test/test-lib.js
+++ test/test-lib.js
@@ -5,35 +5,46 @@ var eventsLib = require("../events-lib");
 
 
 describe("events lib", function() {
-  describe("filterText has value 27", function() {
-    it("should return value 27 if limit is 30", function() {
-      //expect(eventsLib.textFilter("(Vocal, Gitarre, Keyboard, Bass, Drums), Jazz, 10. Bandjubiläum, Vvk 27,90 ",30)).to.be.equal(27.90);
-      expect(eventsLib.textFilter("Vvk 27,90",30)).to.be.equal(27.90);
+  describe("filterText has value 27.90", function() {
+    it("should return value 27.90 if limit is 30", function() {
+      expect(eventsLib.textFilter("(Vocal, Gitarre, Keyboard, Bass, Drums), Jazz, 10. Bandjubiläum, Vvk 27,90 ",30)).to.be.equal(27.90);
+      //expect(eventsLib.textFilter("Vvk 27,90",30)).to.be.equal(27.90);
     });
     it("should return value 100 if limit is 5", function() {
       expect(eventsLib.textFilter("(Vocal, Gitarre, Keyboard, Bass, Drums), Jazz, 10. Bandjubiläum, Vvk 27,90",5)).to.be.equal(100);
     });
   });
-  describe("filter preis ", function() {
+  describe("filter preis out", function() {
     it("should be 100 if price greater limit", function() {
-      expect(eventsLib.textFilter("(Schlagzeug, Abschluss Musikstudium), plus Gesang, 2x Gitarre, Keyboard, Bass, Synthesizer, Percussion, Funk, Soul u.m., Vvk 8,-",5)).to.be.equal(100);
+      expect(eventsLib.textFilter("»Ein Scherz und eine Krone«, Gala-Dinner mit Varieté u. Showprogramm, Vvk ab 138,-. Ort: Spiegelzelt, MesseCity Köln, Deutz-Mülheimer-Str.",5)).to.be.equal(100);
+    });
+    it("should be 100 on text without any price", function() {
+      expect(eventsLib.textFilter("mit Opener Band, Funk, Soul, Jazz",5)).to.be.equal(100);
     });
   });
-  describe("filter preis ", function() {
+  describe("filterText with Erw:", function() {
+    it("should be price 6 if limit is 7", function() {
+      expect(eventsLib.textFilter("mit Julia Kiefer (Schauspiel) u. dem Rubin Quartett (2x Violine, Viola, Cello), Werke von Tschaikowsky, Vivaldi u. Mozart, Philharmonie Veedel Mini, Erw.: 6,-, Kinder ab 1 Jahr 4,-",7)).to.be.equal(6);
+    });
+    it("should be price 100 if limit is 5", function() {
+      expect(eventsLib.textFilter("mit Julia Kiefer (Schauspiel) u. dem Rubin Quartett (2x Violine, Viola, Cello), Werke von Tschaikowsky, Vivaldi u. Mozart, Philharmonie Veedel Mini, Erw.: 6,-, Kinder ab 1 Jahr 4,-",5)).to.be.equal(100);
+    });
+  });
+  describe("not filterText with rubish", function() {
+    it("should not be price 6 if limit is 7", function() {
+      expect(eventsLib.textFilter("mit Julia Kiefer (Schauspiel) u. dem Rubin Quartett (2x Violine, Viola, Cello), Werke von Tschaikowsky, Vivaldi u. Mozart, Philharmonie Veedel Mini, Exx.: 6,-, Kinder ab 1 Jahr 4,-",7)).to.not.equal(6);
+    });
+  });
+  describe("filterText with Ak ", function() {
     it("should be price if price smaller limit", function() {
       expect(eventsLib.textFilter("(Schlagzeug, Abschluss Musikstudium), plus Gesang, 2x Gitarre, Keyboard, Bass, Synthesizer, Percussion, Funk, Soul u.m., Ak 4,-",5)).to.be.equal(4);
     });
   });
-  describe("on text without any price", function() {
-    it("should be 100", function() {
-      expect(eventsLib.textFilter("mit Opener Band, Funk, Soul, Jazz",5)).to.be.equal(100);
-    });
-  });
-  describe("on text with gratis or umsonst", function() {
-    it("should be 0", function() {
+  describe("filter text", function() {
+    it("with gratis should be 0", function() {
       expect(eventsLib.textFilter("mit Opener Band, Funk, Soul, Jazz gratis",5)).to.be.equal(0);
     });
-    it("should be 0", function() {
+    it("with umsonst should be 0", function() {
       expect(eventsLib.textFilter("mit Opener Band, Funk, Soul, Jazz umsonst",5)).to.be.equal(0);
     });  });
   //"mit Julia Kiefer (Schauspiel) u. dem Rubin Quartett (2x Violine, Viola, Cello), Werke von Tschaikowsky, Vivaldi u. Mozart, Philharmonie Veedel Mini, Erw.: 6,-, Kinder ab 1 Jahr 4,-"
